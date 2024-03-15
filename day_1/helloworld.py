74b1a85469f07de29124d60b558c0faf07c320b4
OtavioLira <otavioliraneves@gmail.com>
2024-03-14 23:33:45 -0300

    "Desafio Python Dia 1"

---
+++ day_1/helloworld.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print(3+4) # addition
+print(3-4) # subtraction
+print(3*4) # Multiplication
+print(3%4) # modulos
+print(3/4) # division
+print(3**4) # exponential
+print(3//4) # floor division operator
+
+print("Nivel 3")
+print(10) #Int
+print(7.2) #Float
+print(4 - 5j) # Complex Number
+print("String") # String
+print(True) #Boolean
+print(False) #Boolean
+print({"Set"}) # Set
+print(["Lista"]) # List
+print(("Tupla")) # Tuple
+print({"Dicionario":30}) #Dicionario
+print("Distância euclidiana")
+import math
+print(math.sqrt(((-1 - (-1))**2) + ((0 - 0)**2)))
