69698d81ff5307d468de5ad06f3ea91f5f44ba85
OtavioLira <otavioliraneves@gmail.com>
2024-03-15 22:46:38 -0300

    Dia 2 - Variaveis e funções integradas

---
+++ day_2/variaveis.py
@@ -0,0 +1,38 @@
+#Dia 2: 30 dias de programação python
+primeiro_nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+nome_completo = "Otávio Lira Neves"
+pais = print("Digite seu país: ")
+cidade = "São Paulo"
+idade = print("Digite sua idade: ")
+ano = 2024
+is_married = False
+is_true = True
+is_light_on = True
+var1, var2, var3 = 10,10,10
+
+print(type(primeiro_nome))
+print(type(idade))
+print(type(is_married))
+print(len(primeiro_nome))
+print(len(sobrenome))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two / num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius_of_circle = int(input("Digite o raio do circulo: "))
+pi = 3.14
+area_of_circle = pi * (radius_of_circle ** 2)
+circum_of_circle = 2 * pi * radius_of_circle
+
+print("Area do circulo: ", area_of_circle)
+print("Circunferência do circulo: ", circum_of_circle)
+
+
