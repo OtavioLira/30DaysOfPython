769943ccecbc17a86d897daa7d5eb9682ee64e9b
OtavioLira <=>
2024-03-27 11:48:02 -0300

    Dia 5 - Listas - metodos de listas

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -66,6 +66,158 @@ primeira_fruta, segunda_fruta, terceira_fruta, *resto = frutas
 print(primeira_fruta) # banana
 print(segunda_fruta) # laranja
 print(terceira_fruta) # manga
-print(resto) # ['lima','maça']
+print(resto) # ['limão','lima','maça']
 
+# Segundo Exemplo
+primeiro, segundo, terceiro, *resto, decimo = [1,2,3,4,5,6,7,8,9,10]
+print(primeiro)
+print(segundo)
+print(terceiro)
+print(resto)
+print(decimo)
 
+# Terceiro exemplo
+paises = ["Alemanha","França","Bélgica","Suécia","Dinamarca","Finlândia","Noruega","Islândia","Estônia"]
+alemanha, franca, suecia, *resto, estonia = paises
+print(alemanha)
+print(franca)
+print(suecia)
+print(resto)
+print(estonia)
+
+# Fatiando itens de uma lista
+frutas = ["banana","laranja","manga","limao"]
+todas_frutas = frutas[0:4]
+
+todas_frutas = frutas[0:]
+laranja_manga = frutas[1:3]
+laranja_manga_limao = frutas[1:]
+laranja_limao = frutas[::2]
+
+frutas = ["banana","laranja","manga","limao"]
+todas_frutas = frutas[-4:]
+laranja_manga = frutas[-3:-1]
+laranja_manga_limao = frutas[-3:]
+frutas_reversas = frutas[::-1]
+
+# Modificando listas
+frutas = ["banana","laranja","manga","limao"]
+frutas[0] = "abacate"
+print(frutas)
+frutas[1] = "maca"
+print(frutas)
+ultimo_indice = len(frutas) - 1
+frutas[ultimo_indice] = "lima"
+print(frutas)
+
+# Verificando itens na lista
+frutas = ["banana","laranja","manga","limao"]
+existe = "banana" in frutas
+print(existe) # True
+existe = "lima" in frutas
+print(existe) # False
+
+# Adicionando itens na lista
+# Syntaxe
+
+#lista = list()
+#lista.append(item)
+
+frutas = ["banana","laranja","manga","limao"]
+frutas.append("maca")
+print(frutas)
+frutas.append("lima")
+print(frutas)
+
+# inserindo itens na lista
+
+frutas = ["banana","laranja","manga","limao"]
+frutas.insert(2, "maca")
+print(frutas)
+frutas.insert(3,"lima")
+print(frutas)
+
+# Removendo itens da lista
+frutas = ["banana","laranja","manga","limao"]
+frutas.remove("banana")
+print(frutas)
+frutas.remove("limao")
+print(frutas)
+
+# Removendo itens usando o POP
+frutas = ["banana","laranja","manga","limao"]
+frutas.pop()
+print(frutas)
+
+frutas.pop(0)
+print(frutas)
+
+# Removendo itens usando o DEL
+frutas = ["banana","laranja","manga","limao"]
+del frutas[0]
+print(frutas)
+del frutas[1]
+print(frutas)
+del frutas[1:3]
+print(frutas)
+#del frutas
+#print(frutas)
+
+# Limpando listas
+frutas = ["banana","laranja","manga","limao"]
+frutas.clear()
+print(frutas)
+
+# Copiando uma lista
+frutas = ["banana","laranja","manga","limao"]
+copia_frutas = frutas.copy()
+print(copia_frutas)
+
+# Juntando listas
+numeros_positivos = [1, 2, 3, 4, 5]
+zero =[0]
+numeros_negativos = [-5, -4, -3, -2, -1]
+inteiros = numeros_negativos + zero + numeros_positivos
+print(inteiros)
+frutas = ["banana","laranja","manga","limao"]
+vegetais = ["tomate","batata","repolho","cebola","cenoura"]
+frutas_vegetais = frutas + vegetais
+print(frutas_vegetais)
+
+num1 = [0, 1, 2, 3]
+num2 = [4, 5, 6]
+num1.extend(num2)
+print("Numeros: ", num1)
+numeros_negativos = [-5, -4, -3, -2, -1]
+numeros_positivos = [1, 2, 3, 4, 5]
+zero = [0]
+
+numeros_negativos.extend(zero)
+numeros_positivos.extend(numeros_positivos)
+print("Inteiros: ", numeros_negativos)
+frutas = ["banana","laranja","manga","limao"]
+vegetais = ["tomate","batata","repolho","cebola","cenoura"]
+frutas.extend(vegetais)
+print("Frutas e vegetais: ", frutas)
+
+# Contando itens da lista
+frutas = ["banana","laranja","manga","limao"]
+print(frutas.count("laranja"))
+anos = [22, 19, 24, 25, 26, 24, 25, 24]
+print(anos.count(24))
+
+# Achando o index de um item na lista
+frutas = ["banana","laranja","manga","limao"]
+print(frutas.index("laranja"))
+anos = [22, 19, 24, 25, 26, 24, 25, 24]
+print(anos.index(24))
+
+# invertendo a lista
+frutas = ["banana","laranja","manga","limao"]
+frutas.reverse()
+print(frutas)
+anos = [22, 19, 24, 25, 26, 24, 25, 24]
+anos.reverse()
+print(anos)
+
+# Ordenando tens na lista
